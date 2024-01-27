809ab3a2c1f66fdef985db65670931995796fbba
HiroyaYamazoe <iwtbsos26@gmail.com>
2024-01-27 21:38:50 +0900

    change all.

--- README.md
+++ README.md
@@ -4,9 +4,12 @@ This is an vscode-extension that scrolls the screen by one line when you press E
 
 ## Features
 1. toggleInsertLineAndScroll(Ctrl + Alt + i).
-2. InsertLineAndScroll(Shift + Enter).
+2. InsertLineAndScroll(Enter).
 
 ## Release Notes
+
+### 2.0.0
+Change all.
 
 ### 1.0.0
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,44 +1,41 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { WritableStreamDefaultWriter } from 'stream/web';
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let isEnabled = false;
+let disposable: vscode.Disposable | undefined;
+
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "enter-scroll" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "enter-scroll" is now active!');
+    let toggleDisposable = vscode.commands.registerCommand('enter-scroll.toggleInsertLineAndScroll', () => {
+        isEnabled = !isEnabled;
+       // vscode.window.showInformationMessage(`Insert Line and Scroll is ${isEnabled ? 'enabled' : 'disabled'}`);
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let isEnabled = false;
+        if (isEnabled) {
+            // onDidChangeTextDocument イベントを購読
+            disposable = vscode.workspace.onDidChangeTextDocument(handleTextDocumentChange);
+            vscode.window.showInformationMessage('Insert Line and Scroll is Enabled');
+        } else {
+            disposable?.dispose();
+            vscode.window.showInformationMessage('Insert Line and Scroll is Disabled');
+        }
+    });
 
-	let disposable = vscode.commands.registerCommand('enter-scroll.insertLineAndScroll', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const position = editor.selection.active;
-
-			editor.edit(editBuilder => {
-				editBuilder.insert(position, '\n');
-			}).then(() => {
-				if (isEnabled) {
-					vscode.commands.executeCommand('scrollLineDown');
-				}
-			});
-		}
-	});
-
-	let toggleDisposable = vscode.commands.registerCommand('enter-scroll.toggleInsertLineAndScroll', () => {
-		isEnabled = !isEnabled;
-		vscode.window.showInformationMessage(`Insert Line and Scroll is ${isEnabled ? 'enabled' : 'disabled'}`);
-	});
-
-	context.subscriptions.push(disposable, toggleDisposable);
+    context.subscriptions.push(disposable ?? vscode.Disposable.from(), toggleDisposable);
 }
 
-// This method is called when your extension is deactivated
+function handleTextDocumentChange(event: vscode.TextDocumentChangeEvent) {
+    // テキストが変更されたときに呼び出されるハンドラ
+    const editor = vscode.window.activeTextEditor;
+    if (editor && event.document === editor.document) {
+        // テキストが変更されたドキュメントが現在のエディタと一致する場合のみ処理を実行
+        const lastChange = event.contentChanges[event.contentChanges.length - 1];
+        if (lastChange.text.includes('\n')) {
+            vscode.commands.executeCommand('editorScroll', { to: 'down', by: 'line' });
+        }
+    }
+}
+
 export function deactivate() {}
